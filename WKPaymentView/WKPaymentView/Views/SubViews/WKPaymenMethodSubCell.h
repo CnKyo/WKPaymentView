d9fb9ec6528035d95ffb020baee91db9d2bfb82f
CnKyo <574989766@qq.com>
2019-03-16 19:20:19 +0800

    优化

---
+++ WKPaymentView/WKPaymentView/Views/SubViews/WKPaymenMethodSubCell.h
@@ -0,0 +1,17 @@
+//
+//  WKPaymenMethodSubCell.h
+//  WKPaymentView
+//
+//  Created by apple on 16/03/2019.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKPaymenMethodSubCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WKPaymentView/WKPaymentView/Views/SubViews/WKPaymenMethodSubCell.m
@@ -0,0 +1,24 @@
+//
+//  WKPaymenMethodSubCell.m
+//  WKPaymentView
+//
+//  Created by apple on 16/03/2019.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import "WKPaymenMethodSubCell.h"
+
+@implementation WKPaymenMethodSubCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
